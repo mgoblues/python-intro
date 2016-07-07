637970dc65357299547b7072eacd3313931211de
mgoblues <je60@nyu.edu>
2016-07-07 14:16:31 -0400

    assignment 4 updated loop

--- assignment_4/loop.py
+++ assignment_4/loop.py
@@ -1,3 +1,3 @@
 #print 1-10 on seperate lines
-for i in range(10):
+for i in range(1,11):
 	print(i)
--- assignment_4/loop.sh
+++ assignment_4/loop.sh
@@ -1,6 +1,6 @@
 #!/bash/bin
 
 #print 1-10 on seperate lines
-for i in $(seq 10); do
-	echo "$i"
+for i+1 in $(seq 10); do
+	echo "$i+1"
 done
