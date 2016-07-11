25836b887103c59663d4a765b05eb339e5691fbf
Joel <je60@nyu.edu>
2016-07-11 13:58:08 +0000

    a7

--- assignment_7/a7_source.py
+++ assignment_7/a7_source.py
@@ -20,7 +20,7 @@ TIME_STEP_DELTA = 100
 
 SECONDS_CONVERSION = {
         'minutes': 60,
-        'hours': 60 * 60
+        'hours': 60 * 60,
         'days': 60 * 60 * 24,
         'years': 60 * 60 * 24 * 365
         }
@@ -30,12 +30,13 @@ def force_from_gravity(mass_one, mass_two, distance):
     """ implement the equation calculating force due to gravity from two
     masses and return the force in newtons """
     # ERROR: proper constant name
-    return (GRAVITON * mass_one * mass_two) / (distance * distance)
+    return (GRAVITATION * mass_one * mass_two) / (distance * distance)
 
 
-def escape_velocity(main_mass, distance_from_center):
+def get_escape_velocity(main_mass, distance_from_center):
     """ return the excape velocity at which a craft can overcome the force of gravity """
-    math.sqrt( 2 * GRAVITATION * main_mass / 0 / distance_from_center)
+    return math.sqrt(( 2 * GRAVITATION * main_mass) / distance_from_center)
+        
 
 
 def time_until_destination(time_step, ship_mass, engine_force, target_distance):
@@ -50,11 +51,11 @@ def time_until_destination(time_step, ship_mass, engine_force, target_distance):
     while traveled_distance < target_distance:
         if iterations > MAX_ITERATIONS:
             print("reached max iteration count")
-            print("got ["+ str(traveled_distance / 1000) +"] m")
+            print("got " + traveled_distance + " m")
             break
         elapsed_time += time_step
         ship_velocity += acceleration * time_step
-        traveled_distance += ship_velocity * time_step
+        traveled_distance += (ship_velocity * time_step)
         iterations += 1
     return elapsed_time
 
@@ -65,7 +66,7 @@ def get_readable_time(seconds):
         if remaining_seconds > SECONDS_CONVERSION['years']:
             result['years'] = math.floor(remaining_seconds / SECONDS_CONVERSION['years'])
             remaining_seconds -= result['years'] * SECONDS_CONVERSION['years']
-        elif remaining_seconds > SECONDS_CONVERSION[days]:
+        elif remaining_seconds > SECONDS_CONVERSION['days']:
             result['days'] = math.floor(remaining_seconds / SECONDS_CONVERSION['days'])
             remaining_seconds -= result['days'] * SECONDS_CONVERSION['days']
         elif remaining_seconds > SECONDS_CONVERSION['hours']:
@@ -84,18 +85,18 @@ def get_transit_time(target_solar_orbit_radius):
     at the target radius """
     simplified_distance = math.fabs(target_solar_orbit_radius - EARTH['orbital_radius'])
     time_until_halfway = time_until_destination(TIME_STEP_DELTA, FALCON_9_MASS, EM_DRIVE_FORCE, (simplified_distance / 2))
-    return full_time
     full_time = time_until_halfway * 2
 
+    return full_time
 
 attraction = force_from_gravity(EARTH['mass'], SUN_MASS, EARTH['orbital_radius'])
-print("The force of attraction due to gravity between the earth and the sun is: " + str(attaction))
+print("The force of attraction due to gravity between the earth and the sun is: " + str(attraction))
 
-escape_velocity = escape_velocity(EARTH['mass'], EARTH['radius'])
-print("The escape velocity at the surface of the Earth is: "+str(escape_velocity))
+escape_velocity = get_escape_velocity(EARTH['mass'] , EARTH['radius'])
+print("The escape velocity at the surface of the Earth is: "+ str(escape_velocity) + "m/ s^2")
 
 print("Should see the (idealized) travel time to Saturn given an engine")
-print("with capacity for " + EM_DRIVE_FORCE + " Newtons of constant thrust:")
+print("with capacity for " + str(EM_DRIVE_FORCE) + " Newtons of constant thrust:")
 transit_time = get_transit_time(SATURN_ORBITAL_RADIUS)
 readable_transit = get_readable_time(transit_time)
 print(readable_transit)
